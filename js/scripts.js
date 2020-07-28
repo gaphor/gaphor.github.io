e7f6650caf73d86f80a0e462e103e4c9c15158e2
Arjan Molenaar <gaphor@gmail.com>
2020-07-28 21:44:31 +0200

    Remove unused scripts, clean up css

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,6 @@
  *	jQuery Document Ready
  */
 jQuery(document).ready(function ($) {
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	var documentWidth = $(document).width();
-	var documentHeight = $(document).height();
-
-	// If is IOS
-	function isIsIOS() {
-		$.browser.device = (/iphone|ipad|ipod/i.test(navigator.userAgent.toLowerCase()));
-		if ($.browser.device == true) {
-			$('#counter').css('background-attachment', 'scroll');
-			$('#testimonials').css('background-attachment', 'scroll');
-		}
-	}
 
 	// Smooth Scroll Anchors
 	function smoothScrollAnchors() {
@@ -41,56 +28,10 @@ jQuery(document).ready(function ($) {
 		});
 	}
 
-	// Add Height To Front Page
-	function addHeightToFrontPageProject() {
-		var project = $('#projects .project');
-		var projectWidth = $(project).width();
-
-		$(project).css('height', projectWidth);
-	}
-
-	// Set Color on Front Page Service
-	function setColorOnFrontPagePerson() {
-		if ($('#team .section-content .person').length) {
-			$('#team .section-content .person').each(function () {
-				var person = $(this);
-				var dataPersonColor = $(person).data('person-color');
-				var personPosition = $(person).children('.person-content').children('.person-position');
-				var personContentSocial = $(person).children('.person-content').children('.person-content-social.clearfix').children('li').children('a');
-
-				$(personPosition).css('color', dataPersonColor);
-				$(personContentSocial).css({ 'border-color': dataPersonColor, "color": dataPersonColor });
-			});
-		}
-	}
-
-
 	// Called Functions
 	$(function () {
-		isIsIOS();
 		smoothScrollAnchors();
 		openResponsiveMenu();
-		addHeightToFrontPageProject();
-		setColorOnFrontPagePerson();
 	});
 
-	// Window Resize
-	$(window).resize(function () {
-		// Called Functions
-		$(function () {
-			addHeightToFrontPageProject();
-		});
-	});
 });
-
-if (jQuery('.blog-carousel > .illdy-blog-post').length > 3) {
-	jQuery('.blog-carousel').owlCarousel({
-		'items': 3,
-		'loop': true,
-		'dots': false,
-		'nav': true,
-		'navText': ['<i class="fa fa-angle-left" aria-hidden="true"></i>',
-			'<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-		responsive: { 0: { items: 1 }, 480: { items: 2 }, 900: { items: 3 } }
-	});
-}
