78a9deb8714cbc084b68ac76bec36732afd3b527
Dan Yeaw <dan@yeaw.me>
2023-04-16 20:35:16 -0400

    Remove function and log statement

--- js/languages.js
+++ js/languages.js
@@ -1,22 +1,19 @@
 
-(function() {
-    const chooser = document.getElementById('language-chooser-select');
-    let activeLang = 'en';
-    for (let i = 0; i < chooser.children.length; i++) {
-        if (chooser.children[i].selected) {
-            activeLang = chooser.children[i].value;
-            console.log(activeLang)
-        }
+const chooser = document.getElementById('language-chooser-select');
+let activeLang = 'en';
+for (let i = 0; i < chooser.children.length; i++) {
+    if (chooser.children[i].selected) {
+        activeLang = chooser.children[i].value;
+    }
+}
+
+chooser.onchange = function() {
+    const lang = this.value;
+    let pageUrl = document.location.pathname;
+
+    if (new RegExp('^/' + activeLang + '/').test(pageUrl)) {
+        pageUrl = pageUrl.substring(activeLang.length + 1);
     }
 
-    chooser.onchange = function() {
-        const lang = this.value;
-        let pageUrl = document.location.pathname;
-
-        if (new RegExp('^/' + activeLang + '/').test(pageUrl)) {
-            pageUrl = pageUrl.substring(activeLang.length + 1);
-        }
-
-        document.location = '' + '/' + lang + pageUrl;
-    }
-})();
+    document.location = '' + '/' + lang + pageUrl;
+}
